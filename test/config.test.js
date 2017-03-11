0016448f7e19b993ca2ce925c767eaad30f41d70
penny <penny@penny.lgbt>
2017-03-11 23:55:45 +0100

    add config tests

---
+++ test/config.test.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+import deepmerge from 'deepmerge'
+import config from '../lib/config'
+
+test.only('config is merged correctly', async t => {
+  const conf1 = config.generateConfig()
+  t.deepEqual(conf1, config.DEFAULT_CONFIG)
+
+  const modifier = {
+    exclude: ['deps/'],
+    custom_key: 'test'
+  }
+  const conf2 = config.generateConfig(modifier)
+  t.deepEqual(conf2, deepmerge(config.DEFAULT_CONFIG, modifier))
+})

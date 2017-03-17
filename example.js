1e2ed19bc6565f9b5de8e2499734f8453e8757d9
penny <penny@penny.lgbt>
2017-03-17 13:46:45 +0100

    update example

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
-const koe = require('koe')
+const koe = require('./index')
 const path = require('path')
 
 // define a sample config that overrides/extends some default keys
 const config = {
+  source: 'test/sample',
   destination: path.join(__dirname, 'dest'),
   exclude: [
     'npm-debug.log',
@@ -12,22 +13,13 @@ const config = {
 
 // define a sample middleware
 function middleware (site) {
-  site.log('hey!')
-  site.foo = 'bar'
-}
-
-// you can also hook into specific parts of the build process
-function preconfigHook (site) {
-  site.config.destination = path.join(__dirname, 'dest2')
+  site.files[path.join(__dirname, 'test/sample/test.md')].contents = 'sup'
 }
 
 // the main execution thread. should return a promise
 async function run () {
   const site = koe(config)
   site.use(middleware)
-
-  // use the pre_config hook
-  site.hook('pre_config', preconfigHook)
 
   return site.build()
 }

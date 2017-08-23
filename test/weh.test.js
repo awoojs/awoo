e6e14fbe6b68507223345661dc725091b03c0798
Olivia Hugger <olivia@fastmail.com>
2017-08-23 23:57:28 +0200

    GOD

--- test/weh.test.js
+++ test/weh.test.js
@@ -1,32 +1,6 @@
 const test = require('ava')
 const vfile = require('vfile')
-const rimraf = require('rimraf')
-const fs = require('fs')
-const path = require('path')
 const weh = require('../lib/weh')
-
-test.after.always(t => {
-  rimraf.sync('test/sample_dest')
-})
-
-test('runs on test data', async t => {
-  const plugin = () => {
-    return files => {
-      return files.map(file => Object.assign(file, {contents: 'test'}))
-    }
-  }
-
-  await weh(site => {
-    site.config({
-      source: 'test/sample',
-      destination: 'test/sample_dest'
-    })
-    site.use(plugin)
-    return site
-  })
-
-  t.is(fs.readFileSync(path.join(__dirname, 'sample_dest/test.md'), 'utf-8'), 'test')
-})
 
 test('correctly runs in integration mode', async t => {
   const plugin = () => {

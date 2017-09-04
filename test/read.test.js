856a4f940cf1b5c088b5c775f2becca028ed0955
Olivia Hugger <olivia@fastmail.com>
2017-09-04 14:27:05 +0200

    more function dumbing

--- test/read.test.js
+++ test/read.test.js
@@ -2,36 +2,22 @@ const test = require('ava')
 const read = require('../lib/read')
 
 test('reads files correctly', async t => {
-  const config = {
-    source: 'test/sample',
-    exclude: []
-  }
-
-  const res = await read(config)
+  const res = await read('test/sample')
 
   const file = res.find(f => f.path === 'test.md')
   t.true(file.contents.trim() === 'Hello!')
 })
 
 test('reads binary files as buffers', async t => {
-  const config = {
-    source: 'test/sample',
-    exclude: []
-  }
-
-  const res = await read(config)
+  const res = await read('test/sample')
   const file = res.find(f => f.path === 'picture.png')
   t.true(Buffer.isBuffer(file.contents))
   t.true(Buffer.byteLength(file.contents) === 1119)
 })
 
 test('throws on nonexistent path', async t => {
-  const config = {
-    source: 'test/fakepath'
-  }
-
   try {
-    await read(config)
+    await read('test/fakepath')
   } catch (err) {
     t.truthy(err[0])
     t.is(err[0].code, 'ENOENT')

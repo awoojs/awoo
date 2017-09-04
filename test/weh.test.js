36c08f38c7618e3cf4822299a10188501fca97e9
Olivia Hugger <olivia@fastmail.com>
2017-09-04 14:48:50 +0200

    add no write test

--- test/weh.test.js
+++ test/weh.test.js
@@ -2,6 +2,29 @@ const test = require('ava')
 const vfile = require('vfile')
 const weh = require('../lib/weh')
 
+test('works correctly', async t => {
+  const plugin = () => {
+    return files => files.map(file => {
+      file.contents = 'haha test'
+      return file
+    })
+  }
+
+  const res = await weh(site => {
+    site.config({
+      source: 'test/sample',
+      no_write: true
+    })
+
+    site.use(plugin)
+    return site
+  })
+
+  t.log(res.files)
+  t.log(res.files.map(f => f.basename))
+  t.is(res.files.find(f => f.basename === 'test.md').contents, 'haha test')
+})
+
 test('correctly runs in integration mode', async t => {
   const plugin = () => {
     return files => {
